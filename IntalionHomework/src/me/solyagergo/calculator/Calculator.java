63040b28d6f2c53f778b01b4f91e43ee3d95d069
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-09-17 18:15:22 +0200

    add: base calculator operations

--- IntalionHomework/src/me/solyagergo/calculator/Calculator.java
+++ IntalionHomework/src/me/solyagergo/calculator/Calculator.java
@@ -1,5 +1,23 @@
 package me.solyagergo.calculator;
 
 public class Calculator {
+	
+	public double addition(double operandOne, double operandTwo) {
+		return operandOne + operandTwo;
+	}
+	
+	public double subtraction(double operandOne, double operandTwo) {
+		return operandOne + operandTwo;
+	}
+	
+	public double multiplication(double operandOne, double operandTwo) {
+		return operandOne * operandTwo;
+	}
+	
+	public double division(double operandOne, double operandTwo) {
+		//If you divide by zero with a double the result will be Infinity, therefore we need to check for the exception manually.
+		if (operandTwo == 0) throw new ArithmeticException("Can't divide by zero");
+		return operandOne / operandTwo;
+	}
 
 }
